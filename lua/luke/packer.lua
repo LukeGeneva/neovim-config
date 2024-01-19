c2519c0383cca3e8ead9208084ef6ebdf285b6d0
Luke Geneva <lukegeneva@gmail.com>
2024-01-18 22:07:23 -0600

    Add quick command for LSP restart

--- lua/luke/packer.lua
+++ lua/luke/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
@@ -21,8 +21,6 @@ return require('packer').startup(function(use)
 	  end
   })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
 
