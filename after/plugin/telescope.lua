a28f68ef69940757cd6729c867103f55446b275c
Luke Geneva <lukegeneva@gmail.com>
2024-01-24 15:52:26 -0600

    Include hidden files in find files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,15 @@
+require('telescope').setup{
+  defaults = {
+    -- other default configurations
+  },
+  pickers = {
+    find_files = {
+      find_command = {'rg', '--files', '--hidden', '--glob', '!.git/*'}
+    }
+  },
+  -- other setups
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
