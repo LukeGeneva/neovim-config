5394361baa99754ffaacf33bfb87f97575a22732
Luke Geneva <lukegeneva@gmail.com>
2024-08-11 19:20:38 -0500

    Do not format async

--- lua/luke/set.lua
+++ lua/luke/set.lua
@@ -82,11 +82,9 @@ vim.api.nvim_create_autocmd(
   { pattern = { "text", "markdown" }, command = "setlocal spell" }
 )
 
-vim.api.nvim_create_augroup('AutoFormatting', {})
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = {'*.go', '*.html'},
-  group = 'AutoFormatting',
   callback = function()
-    vim.lsp.buf.format({ async = true })
+    vim.lsp.buf.format()
   end,
 })
